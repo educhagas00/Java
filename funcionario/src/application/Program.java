d22e13bbc1066d165b01f2a5781850851df2a134
educhagas00 <educhagas00@gmail.com>
2023-05-25 23:17:19 -0300

    exercicio salario 1.0

---
+++ funcionario/src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Employee;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee emp = new Employee();
+		
+		emp.setName(sc.nextLine());
+		emp.setGrossSalary(sc.nextDouble());
+		emp.setTax(sc.nextDouble());
+		
+		System.out.println("Name: " + emp.getName());
+		System.out.println("Gross Salary: " + emp.getGrossSalary());
+		System.out.println("Tax: " +emp.getTax());
+		
+		System.out.println("");
+		
+		System.out.println("Employee: " + emp.getName() + ", $ " + emp.netSalary());
+		
+		System.out.println("");
+		
+		System.out.println("Which percentage to increase salary?");
+		double percentage = sc.nextDouble();
+		
+		emp.increaseSalary(percentage);
+		
+		System.out.println("Updated data: " + emp.getName() + ", $ " + emp.increaseSalary(percentage));
+		
+		sc.close();
+	}
+
+}
