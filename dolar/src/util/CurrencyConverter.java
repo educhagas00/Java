33c3a24ac849a13d02625cf101f52dce4e18f91b
educhagas00 <educhagas00@gmail.com>
2023-06-01 21:08:32 -0300

    exercicio dollar converter 1.1

--- dolar/src/util/CurrencyConverter.java
+++ dolar/src/util/CurrencyConverter.java
@@ -2,10 +2,10 @@ package util;
 
 public class CurrencyConverter {
 	
-	public static double IOF = 0.6;
+	public static double IOF = 0.06;
 	
 	public static double calculator(double dollar, double quantity) {
-		return dollar * quantity * (IOF);
+		return dollar * quantity * (IOF + 1);
 	}
 
 }
