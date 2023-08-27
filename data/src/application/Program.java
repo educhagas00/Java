a40a52e8f0f9e5a1db78874f95e3e690aad025e7
educhagas00 <educhagas00@gmail.com>
2023-08-27 16:32:27 -0300

    feature: some functions of date class

--- data/src/application/Program.java
+++ data/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class Program {
@@ -16,15 +17,18 @@ public class Program {
 		
 		//agora -> data-hora ;
 		
-		LocalDate d01 = LocalDate.now();
-		LocalDateTime d02 = LocalDateTime.now(); // com segundos ;
+		LocalDate hoje = LocalDate.now();
+		LocalDateTime agora = LocalDateTime.now(); // com segundos ;
 		Instant d03 = Instant.now(); // horario global com padrao GMT ;
 		
-		//padrao ISO -> data-hora ;
+		//padrao ISO 8601 -> data-hora ;
 		
 		LocalDate d04 = LocalDate.parse("2022-03-16");
+		
 		LocalDateTime d05 = LocalDateTime.parse("2022-03-16T15:30:40");
-		Instant d06 = Instant.parse("2023-06-16T15:30:40Z");
+		
+		Instant d06 = Instant.parse("2023-06-16T01:30:40Z");
+		
 		Instant d07 = Instant.parse("2023-06-16T15:30:40-03:00");
 		
 		//formato customizado -> data-hora ;
@@ -37,16 +41,36 @@ public class Program {
 		
 		LocalDate d10 = LocalDate.of(2023, 7, 15);
 		
-		System.out.println("d01 = " + d01);
-		System.out.println("d02 = " + d02);
-		System.out.println("d03 = " + d03);
-		System.out.println("d04 = " + d04);
-		System.out.println("d05 = " + d05);
-		System.out.println("d06 = " + d06);
-		System.out.println("d07 = " + d07);
-		System.out.println("d08 = " + d08);
-		System.out.println("d09 = " + d09);
-		System.out.println("d10 = " + d10);
+	
+		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d04 = " + fmt1.format(d04));
+		
+		System.out.println("----------------------------");
+		
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm").withZone(ZoneId.systemDefault()); //timezone do pc
+		
+		System.out.println("d06 = " + fmt3.format(d06)); // quando for instant, deve formatar especificando o fuso
+		
+		System.out.println("----------------------------");
+		
+		Instant dia = Instant.parse("2023-07-15T13:30:10Z");
+		
+		LocalDateTime r1 = LocalDateTime.ofInstant(dia, ZoneId.of("Portugal"));
+		
+		System.out.println("r1 = " + r1);
+		
+		System.out.println("----------------------------");
+		
+		System.out.println("dia de hoje: " + hoje.getDayOfMonth());
+		System.out.println("estamos no mes: " + hoje.getMonthValue());
+		System.out.println("estamos no ano: " + hoje.getYear());
+		
+		System.out.println("horas: " + agora.getHour());
+		System.out.println("minutos: " + agora.getMinute());
+		System.out.println("segundos: " + agora.getSecond());
+		
+		System.out.println("----------------------------");
+		                                                             
 	}
 
 }
